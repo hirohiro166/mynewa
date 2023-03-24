0963832ec408246e80ef1d1e028b53df3e4f20db
hiromu <h.tsuchida069@jutjoy.com>
2023-03-24 22:00:24 +0900

    MAMP

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\Admin\NewsController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -27,10 +26,6 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-Route::controller(NewsController::class)->prefix('admin')->middleware('auth')->name('news.')->group(function(){
-    Route::get('news/create', 'add')->name('add');
 });
 
 require __DIR__.'/auth.php';
